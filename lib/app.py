be2d0eee1c2186f2ba27d992d3fb41173bebd961
Gi-thinji <githinji.ngare@gmail.com>
2023-08-16 13:55:53 +0300

    passed tests

---
+++ lib/app.py
@@ -0,0 +1,8 @@
+#lib/app.py
+print("Hello World! Pass this test, please.")
+# print("Hello sun!")
+# print("Hello sky!")
+
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
